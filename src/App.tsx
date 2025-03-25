90ac57b3275ad7e211886f10a46f00a84beb516d
Chitranssh <saxenaayush2052001@gmail.com>
2025-03-25 22:46:36 +0530

    adds useauth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
 import { useKindeAuth } from '@kinde-oss/kinde-auth-react'
+import useAuth from './useAuth';
 
 
 function App() {
+  const {isAuthLoading, isAuthenticated, user} = useAuth();
+  console.log(isAuthLoading, isAuthenticated);
+  console.log(user);
   const {login, logout,  isAuthenticated : isKindeAuthenticated , isLoading : isKindeAuthLoading ,  user: kindeUser } = useKindeAuth();
   console.log(isKindeAuthLoading, isKindeAuthenticated)
   console.log("user is : ", kindeUser);
